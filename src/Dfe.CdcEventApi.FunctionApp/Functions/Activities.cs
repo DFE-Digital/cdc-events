0f52520aedcf8fc13e21e1acae61819fb38c4880
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-03-08 11:01:35 +0000

    Some more front-facing functions.

---
+++ src/Dfe.CdcEventApi.FunctionApp/Functions/Activities.cs
@@ -0,0 +1,65 @@
+﻿namespace Dfe.CdcEventApi.FunctionApp.Functions
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Dfe.CdcEventApi.Application.Definitions;
+    using Dfe.CdcEventApi.Application.Models;
+    using Dfe.CdcEventApi.Domain.Definitions;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+
+    /// <summary>
+    /// Entry class for the <c>activities</c> function.
+    /// </summary>
+    public class Activities : FunctionsBase
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Activities" />
+        /// class.
+        /// </summary>
+        /// <param name="entityProcessor">
+        /// An instance of type <see cref="IEntityProcessor" />.
+        /// </param>
+        /// <param name="loggerProvider">
+        /// An instance of type <see cref="ILoggerProvider" />.
+        /// </param>
+        public Activities(
+            IEntityProcessor entityProcessor,
+            ILoggerProvider loggerProvider)
+            : base(
+                  entityProcessor,
+                  loggerProvider)
+        {
+            // Nothing for now.
+        }
+
+        /// <summary>
+        /// Entry method for the <c>activities</c> function.
+        /// </summary>
+        /// <param name="httpRequest">
+        /// An instance of type <see cref="HttpRequest" />.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// An instance of <see cref="CancellationToken" />.
+        /// </param>
+        /// <returns>
+        /// An instance of type <see cref="IActionResult" />.
+        /// </returns>
+        [FunctionName("activities")]
+        public async Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "POST")]
+            HttpRequest httpRequest,
+            CancellationToken cancellationToken)
+        {
+            IActionResult toReturn =
+                await this.RunAsync<Activity>(
+                    httpRequest,
+                    cancellationToken)
+                .ConfigureAwait(false);
+
+            return toReturn;
+        }
+    }
+}
