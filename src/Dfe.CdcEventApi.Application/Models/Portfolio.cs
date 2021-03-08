5d11100fc05bea82da169586c6654c4481c84a2d
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-03-08 10:37:43 +0000

    Function entry / portfolios.

---
+++ src/Dfe.CdcEventApi.Application/Models/Portfolio.cs
@@ -0,0 +1,10 @@
+﻿namespace Dfe.CdcEventApi.Application.Models
+{
+    /// <summary>
+    /// Represents a <c>portfolio</c> entity.
+    /// </summary>
+    public class Portfolio : ModelsBase
+    {
+        // Nothing for now.
+    }
+}
