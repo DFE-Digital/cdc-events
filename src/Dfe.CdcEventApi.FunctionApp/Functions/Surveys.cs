5ab4ec4864fb92d63c49acb314c3dfec59959aae
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-03-08 12:09:40 +0000

    All remaining functions for collecting event data.

---
+++ src/Dfe.CdcEventApi.FunctionApp/Functions/Surveys.cs
@@ -0,0 +1,64 @@
+﻿namespace Dfe.CdcEventApi.FunctionApp.Functions
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Dfe.CdcEventApi.Application.Definitions;
+    using Dfe.CdcEventApi.Application.Models;
+    using Dfe.CdcEventApi.Domain.Definitions;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+
+    /// <summary>
+    /// Entry class for the <c>surveys</c> function.
+    /// </summary>
+    public class Surveys : FunctionsBase
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Surveys" /> class.
+        /// </summary>
+        /// <param name="entityProcessor">
+        /// An instance of type <see cref="IEntityProcessor" />.
+        /// </param>
+        /// <param name="loggerProvider">
+        /// An instance of type <see cref="ILoggerProvider" />.
+        /// </param>
+        public Surveys(
+            IEntityProcessor entityProcessor,
+            ILoggerProvider loggerProvider)
+            : base(
+                  entityProcessor,
+                  loggerProvider)
+        {
+            // Nothing for now.
+        }
+
+        /// <summary>
+        /// Entry method for the <c>surveys</c> function.
+        /// </summary>
+        /// <param name="httpRequest">
+        /// An instance of type <see cref="HttpRequest" />.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// An instance of <see cref="CancellationToken" />.
+        /// </param>
+        /// <returns>
+        /// An instance of type <see cref="IActionResult" />.
+        /// </returns>
+        [FunctionName("surveys")]
+        public async Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "POST")]
+            HttpRequest httpRequest,
+            CancellationToken cancellationToken)
+        {
+            IActionResult toReturn =
+                await this.RunAsync<Survey>(
+                    httpRequest,
+                    cancellationToken)
+                .ConfigureAwait(false);
+
+            return toReturn;
+        }
+    }
+}
