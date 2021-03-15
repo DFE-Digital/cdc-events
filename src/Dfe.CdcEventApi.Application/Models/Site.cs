9d31e79e35e54ff8a1966b42ca29994b08698f45
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-03-15 10:04:57 +0000

    Updated site scripts.

--- src/Dfe.CdcEventApi.Application/Models/Site.cs
+++ src/Dfe.CdcEventApi.Application/Models/Site.cs
@@ -7,13 +7,13 @@
     /// <summary>
     /// Represents an individual <c>sites</c> entity.
     /// </summary>
-    [DataHandler("CreateSites")]
+    [DataHandler("Create_Raw_Site")]
     public class Site : ModelsBase
     {
         /// <summary>
         /// Gets or sets the <c>Fields</c> property.
         /// </summary>
-        [DataHandler("CreateSitesCustomFields")]
+        [DataHandler("Create_Raw_Site_Field")]
         [JsonProperty("Fields")]
         public IEnumerable<CustomField> Fields
         {
