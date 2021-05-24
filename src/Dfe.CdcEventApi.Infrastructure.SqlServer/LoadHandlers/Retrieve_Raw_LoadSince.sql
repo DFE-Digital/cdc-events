5038c8e37568e0507494407c11007020f6ff5369
Iain Stevenson <iain.stevenson@digital.education.gov.uk>
2021-05-24 07:39:27 +0100

    Fix since issue

--- src/Dfe.CdcEventApi.Infrastructure.SqlServer/LoadHandlers/Retrieve_Raw_LoadSince.sql
+++ src/Dfe.CdcEventApi.Infrastructure.SqlServer/LoadHandlers/Retrieve_Raw_LoadSince.sql
@@ -8,20 +8,30 @@ SELECT
 FROM
 	[dbo].[Raw_Load]
 WHERE 
-	 [Load_DateTime]  BETWEEN DATEADD(ms,-5, @RunIdentifier) AND DATEADD(ms,5, @RunIdentifier)
+	 [Load_DateTime]  = @RunIdentifier
 UNION
-SELECT TOP 1
-	[Load_DateTime],
-	[Finish_DateTime],
-	[Status],
-	[ReportTitle],
-	[ReportBody],
-	[ReportedTo]
+SELECT 
+	load.[Load_DateTime] AS [Load_DateTime],
+	load.[Finish_DateTime] AS [Finish_DateTime],
+	load.[Status] AS [Status],
+	load.[ReportTitle] AS [ReportTitle],
+	load.[ReportBody] AS [ReportBody],
+	load.[ReportedTo] AS [ReportedTo]
 FROM
-	[dbo].[Raw_Load]
-WHERE 
-	 [Load_DateTime]  < DATEADD(ms,-5, @RunIdentifier)
-AND
-	[Status] = 32
+	[dbo].[Raw_Load] load
+INNER JOIN 
+	 (
+		 SELECT 
+			MAX( [Load_DateTime] ) AS [Load_DateTime], [Status]
+		 FROM
+			[dbo].[Raw_Load] 
+		WHERE 
+			[Load_DateTime] < @RunIdentifier
+		GROUP BY 
+			[Status]
+		HAVING
+			[Status] = 32
+	) latest
+ON load.[Load_DateTime] = latest.[Load_DateTime]
 ORDER BY
 	[Load_DateTime] DESC
