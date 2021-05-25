2516b2892d30764137f5123f942dacbeb84c70f0
IainStevenson-DfE <83961058+IainStevenson-DfE@users.noreply.github.com>
2021-05-25 20:55:04 +0100

    Fix blob creation SQL (#20)

--- src/Dfe.CdcEventApi.Infrastructure.SqlServer/DataHandlers/Create_Raw_Blob.sql
+++ src/Dfe.CdcEventApi.Infrastructure.SqlServer/DataHandlers/Create_Raw_Blob.sql
@@ -1,10 +1,10 @@
 INSERT INTO [Raw_Blob]
-    ([Blobkey], [ParentObjectType], [ParentObjectId], [URL], [EntityStatus], [Load_DateTime])
+    ([BlobKey], [ParentObjectType], [ParentObjectId], [BlobData], [MimeType], [Load_DateTime])
 SELECT
-    Tbl.Col.value('Blobkey[1]', 'nvarchar(40)') AS [Blobkey],
+    Tbl.Col.value('BlobKey[1]', 'nvarchar(40)') AS [BlobKey],
     Tbl.Col.value('ParentObjectType[1]', 'nvarchar(40)') AS [ParentObjectType],
     Tbl.Col.value('ParentObjectId[1]', 'nvarchar(40)') AS [ParentObjectId],
-    Tbl.Col.value('BlobData[1]', 'nbinary(max)') AS [BlobData],
+    Tbl.Col.value('BlobData[1]', 'nvarchar(max)') AS [BlobData],
     Tbl.Col.value('MimeType[1]', 'nvarchar(100)') AS [MimeType],
     @RunIdentifier as [Load_DateTime] 
 FROM @Entities.nodes('//Entity') Tbl(Col);
