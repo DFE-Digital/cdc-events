06cdda25401ce9f5c7cbfc1b510cc1e8c46041e8
IainStevenson-DfE <83961058+IainStevenson-DfE@users.noreply.github.com>
2021-06-28 13:27:23 +0100

    Fix to Attachment class to surface the additional fields now requried. (#50)

--- src/Dfe.CdcEventApi.Domain/Models/Attachment.cs
+++ src/Dfe.CdcEventApi.Domain/Models/Attachment.cs
@@ -6,9 +6,19 @@
     public class Attachment
     {
         /// <summary>
+        /// Gets or sets a value for the SiteUniqueId.
+        /// </summary>
+        public string SiteUniqueId { get; set; }
+
+        /// <summary>
         /// Gets or sets a value for the blob key.
         /// </summary>
         public string BlobKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value for the MimeType.
+        /// </summary>
+        public string MimeType { get; set; }
 
         /// <summary>
         /// Gets or sets a value for the folder.
