1ad97cee9ae29ed67c9b014c6b229891f20cb075
IainStevenson-DfE <83961058+IainStevenson-DfE@users.noreply.github.com>
2021-07-23 15:21:37 +0100

    83000 refactoring (#69)
    * WIP: unprepared changes
    
    * WIP: untested- latest working logic app
    
    * WIP: hard coded parameters
    
    * Logic app update and script changes for cotnrol states
    
    * Latest changes to logic app
    
    * Fixes
    
    * Fixed json an removed redundant file
    
    * Fixed Integer parameters
    
    * Fixed missing section
    
    * Fixed secrets parameters
    
    * Fix target endpoint Uri's
    
    * Fix problems handling ControlState
    
    * Fix mistake in status setting
    
    * Embedded re-login for source during entities step
    
    * Fix data Uri
    
    * Fic to target endpoint uri

--- src/event-generator-template.ps1
+++ src/event-generator-template.ps1
@@ -7,7 +7,7 @@ $a.resources[0].properties.definition.parameters.SourceOAUTHKeyUri.defaultValue 
 $a.resources[0].properties.definition.parameters.SourceOAUTHSecretUri.defaultValue = "[concat(reference(variables('keyVaultSecretKycloudApiPassword'), '2019-09-01').secretUriWithVersion, '?api-version=2016-10-01')]"
 $a.resources[0].properties.definition.parameters.TargetOAUTHCredentialsUri.defaultValue = "[concat(reference(variables('keyVaultSecretCdcEventsTokenRequestPayload'), '2019-09-01').secretUriWithVersion, '?api-version=2016-10-01')]"
 $a.resources[0].properties.definition.parameters.TargetOAUTHKeyUri.defaultValue = "[concat(reference(variables('keyVaultSecretCdcEventsSubscriptionKey'), '2019-09-01').secretUriWithVersion, '?api-version=2016-10-01')]"
-$a.resources[0].properties.definition.parameters.TargetEndpoint.defaultValue = "[parameters('cdcEventsApiBaseUri')]"
+$a.resources[0].properties.definition.parameters.TargetEndpoint.defaultValue = "[concat(parameters('cdcEventsApiBaseUri'), '/')]"
 $a.resources[0].properties.definition.parameters.TargetControlEndpoint.defaultValue = "[concat(parameters('cdcEventsApiBaseUri'), '/load')]"
 $a.resources[0].properties.definition.parameters.TargetOAUTHEndpoint.defaultValue = "[parameters('internalOAuthTokenEndpoint')]"
 $a.resources[0].properties.definition.parameters.StatusStart.defaultValue = 1
