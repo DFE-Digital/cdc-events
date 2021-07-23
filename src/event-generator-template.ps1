22df81af86534cb4b6c65a086a9922e684bb368a
IainStevenson-DfE <83961058+IainStevenson-DfE@users.noreply.github.com>
2021-07-23 10:47:51 +0100

    83000 refactoring (#65)
    * WIP: unprepared changes
    
    * WIP: untested- latest working logic app
    
    * WIP: hard coded parameters
    
    * Logic app update and script changes for cotnrol states
    
    * Latest changes to logic app
    
    * Fixes
    
    * Fixed json an removed redundant file
    
    * Fixed Integer parameters
    
    * Fixed missing section
    
    * Fixed secrets parameters

--- src/event-generator-template.ps1
+++ src/event-generator-template.ps1
@@ -3,13 +3,13 @@ $a = Get-Content $pathToJson | ConvertFrom-Json
 $a.resources[0].properties.definition.parameters.SourceEndpoint.defaultValue = "[variables('dataPathUri')]"
 $a.resources[0].properties.definition.parameters.SourceOAUTHEndpoint.defaultValue = "[variables('loginUri')]"
 $a.resources[0].properties.definition.parameters.SourceOAUTHId.defaultValue = "[parameters('kycloudApiEmail')]"
-$a.resources[0].properties.definition.parameters.SourceOAUTHKeyUri.defaultValue = "[variables('keyVaultSecretKycloudApiToken')]"
-$a.resources[0].properties.definition.parameters.SourceOAUTHSecretUri.defaultValue = "[variables('keyVaultSecretKycloudApiPassword')]"
-$a.resources[0].properties.definition.parameters.TargetControlEndpoint.defaultValue = "[concat(variables('dataPathUri'),'/','load')]"
+$a.resources[0].properties.definition.parameters.SourceOAUTHKeyUri.defaultValue = "[concat(reference(variables('keyVaultSecretKycloudApiToken'), '2019-09-01').secretUriWithVersion, '?api-version=2016-10-01')]"
+$a.resources[0].properties.definition.parameters.SourceOAUTHSecretUri.defaultValue = "[concat(reference(variables('keyVaultSecretKycloudApiPassword'), '2019-09-01').secretUriWithVersion, '?api-version=2016-10-01')]"
+$a.resources[0].properties.definition.parameters.TargetOAUTHCredentialsUri.defaultValue = "[concat(reference(variables('keyVaultSecretCdcEventsTokenRequestPayload'), '2019-09-01').secretUriWithVersion, '?api-version=2016-10-01')]"
+$a.resources[0].properties.definition.parameters.TargetOAUTHKeyUri.defaultValue = "[concat(reference(variables('keyVaultSecretCdcEventsSubscriptionKey'), '2019-09-01').secretUriWithVersion, '?api-version=2016-10-01')]"
 $a.resources[0].properties.definition.parameters.TargetEndpoint.defaultValue = "[variables('dataPathUri')]"
-$a.resources[0].properties.definition.parameters.TargetOAUTHCredentialsUri.defaultValue = "[variables('keyVaultSecretCdcEventsTokenRequestPayload')]"
+$a.resources[0].properties.definition.parameters.TargetControlEndpoint.defaultValue = "[concat(variables('dataPathUri'),'/','load')]"
 $a.resources[0].properties.definition.parameters.TargetOAUTHEndpoint.defaultValue = "[parameters('internalOAuthTokenEndpoint')]"
-$a.resources[0].properties.definition.parameters.TargetOAUTHKeyUri.defaultValue = "[variables('keyVaultSecretCdcEventsSubscriptionKey')]"
 $a.resources[0].properties.definition.parameters.StatusStart.defaultValue = 1
 $a.resources[0].properties.definition.parameters.StatusLogin.defaultValue = 2
 $a.resources[0].properties.definition.parameters.StatusEntities.defaultValue = 3
