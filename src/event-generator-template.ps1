3bd958f43a4c2ff77da8583653fabec68adee215
Iain Stevenson <iain.stevenson@digital.education.gov.uk>
2021-07-23 11:10:55 +0100

    Fix target endpoint Uri's

--- src/event-generator-template.ps1
+++ src/event-generator-template.ps1
@@ -7,8 +7,8 @@ $a.resources[0].properties.definition.parameters.SourceOAUTHKeyUri.defaultValue 
 $a.resources[0].properties.definition.parameters.SourceOAUTHSecretUri.defaultValue = "[concat(reference(variables('keyVaultSecretKycloudApiPassword'), '2019-09-01').secretUriWithVersion, '?api-version=2016-10-01')]"
 $a.resources[0].properties.definition.parameters.TargetOAUTHCredentialsUri.defaultValue = "[concat(reference(variables('keyVaultSecretCdcEventsTokenRequestPayload'), '2019-09-01').secretUriWithVersion, '?api-version=2016-10-01')]"
 $a.resources[0].properties.definition.parameters.TargetOAUTHKeyUri.defaultValue = "[concat(reference(variables('keyVaultSecretCdcEventsSubscriptionKey'), '2019-09-01').secretUriWithVersion, '?api-version=2016-10-01')]"
-$a.resources[0].properties.definition.parameters.TargetEndpoint.defaultValue = "[variables('dataPathUri')]"
-$a.resources[0].properties.definition.parameters.TargetControlEndpoint.defaultValue = "[concat(variables('dataPathUri'),'/','load')]"
+$a.resources[0].properties.definition.parameters.TargetEndpoint.defaultValue = "[parameters('cdcEventsApiBaseUri')]"
+$a.resources[0].properties.definition.parameters.TargetControlEndpoint.defaultValue = "[concat(parameters('cdcEventsApiBaseUri'), '/load')]"
 $a.resources[0].properties.definition.parameters.TargetOAUTHEndpoint.defaultValue = "[parameters('internalOAuthTokenEndpoint')]"
 $a.resources[0].properties.definition.parameters.StatusStart.defaultValue = 1
 $a.resources[0].properties.definition.parameters.StatusLogin.defaultValue = 2
