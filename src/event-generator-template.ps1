b819950f1c17c81188f33f7d9dacf2c2f2683e5b
swalters-dfe <steve.walters@education.gov.uk>
2022-02-16 09:08:44 +0000

    Enhanced logging, storage api connection and retry behaviour

--- src/event-generator-template.ps1
+++ src/event-generator-template.ps1
@@ -14,15 +14,28 @@ $a.resources[0].properties.definition.parameters.TargetEndpoint.defaultValue = "
 $a.resources[0].properties.definition.parameters.TargetControlEndpoint.defaultValue = "[concat(parameters('cdcEventsApiBaseUri'), '/control')]"
 $a.resources[0].properties.definition.parameters.TargetOAUTHEndpoint.defaultValue = "[parameters('internalOAuthTokenEndpoint')]"
 $a.resources[0].properties.definition.parameters.CDC2NotifyAPIKeySecretUri.defaultValue  = "[concat(reference(variables('keyVaultSecretCDC2NotifyAPIKey'), '2019-09-01').secretUriWithVersion, '?api-version=2016-10-01')]"
+#Log Analytics Connection
 $a.resources[0].properties.parameters.'$connections'.value.azureloganalyticsdatacollector.connectionId = "[concat('/subscriptions/', parameters('subscriptionId'), '/resourceGroups/', parameters('resourceGroupPrefix'), '-cdcaeg/providers/Microsoft.Web/connections/', parameters('resourceGroupPrefix'), '-logapi-01')]"
 $a.resources[0].properties.parameters.'$connections'.value.azureloganalyticsdatacollector.connectionName = "[concat(parameters('resourceGroupPrefix'), '-logapi-01')]"
-$a.resources[0].properties.parameters.'$connections'.value.azureloganalyticsdatacollector.id = "[concat('/subscriptions/', parameters('subscriptionId'), '/providers/Microsoft.Web/locations/westeurope/managedApis/', parameters('resourceGroupPrefix'), '-logapi-01')]"
+$a.resources[0].properties.parameters.'$connections'.value.azureloganalyticsdatacollector.id = "[concat('/subscriptions/', parameters('subscriptionId'), '/providers/Microsoft.Web/locations/westeurope/managedApis/azureloganalyticsdatacollector')]"
+#File Storage Connection
+$a.resources[0].properties.parameters.'$connections'.value.azurefile.connectionId = "[concat('/subscriptions/', parameters('subscriptionId'), '/resourceGroups/', parameters('resourceGroupPrefix'), '-cdcaeg/providers/Microsoft.Web/connections/', parameters('resourceGroupPrefix'), '-fileapi-01')]"
+$a.resources[0].properties.parameters.'$connections'.value.azurefile.connectionName = "[concat(parameters('resourceGroupPrefix'), '-fileapi-01')]"
+$a.resources[0].properties.parameters.'$connections'.value.azurefile.id = "[concat('/subscriptions/', parameters('subscriptionId'), '/providers/Microsoft.Web/locations/westeurope/managedApis/azurefile')]"
 
 # The body template for sending log analytics has to be escaped here otherwise it all goes wrong when the json is unescaped in the last step
 # Needs to be replicated if further log analytics steps are added
-$sendDataBodyTemplate = $a.resources[0].properties.definition.actions.'Scope-Attachments'.actions.'For-Each-Attachment'.actions.'Validate-Post-Attachment'.else.actions.'Send_Data'.inputs.body
-$sendDataBodyTemplate = $sendDataBodyTemplate.Replace('"', '\"').Replace("`n","").Replace("`r","")
-$a.resources[0].properties.definition.actions.'Scope-Attachments'.actions.'For-Each-Attachment'.actions.'Validate-Post-Attachment'.else.actions.'Send_Data'.inputs.body = $sendDataBodyTemplate
+$logFailedAttachmentBodyTemplate = $a.resources[0].properties.definition.actions.'Scope-Attachments'.actions.'For-Each-Attachment'.actions.'Validate-Post-Attachment'.else.actions.'Log-Failed-Attachment'.inputs.body
+$logFailedAttachmentBodyTemplate = $logFailedAttachmentBodyTemplate.Replace('"', '\"').Replace("`n","").Replace("`r","")
+$a.resources[0].properties.definition.actions.'Scope-Attachments'.actions.'For-Each-Attachment'.actions.'Validate-Post-Attachment'.else.actions.'Log-Failed-Attachment'.inputs.body = $logFailedAttachmentBodyTemplate
+
+$logGetEntityPageBodyTemplate = $a.resources[0].properties.definition.actions.'Scope-Entities'.actions.'For-Each-Entity'.actions.'Until-Control-Entity-Completed'.actions.'For-Each-Page'.actions.'Log-Get-Entity-Page'.inputs.body
+$logGetEntityPageBodyTemplate = $logGetEntityPageBodyTemplate.Replace('"', '\"').Replace("`n","").Replace("`r","")
+$a.resources[0].properties.definition.actions.'Scope-Entities'.actions.'For-Each-Entity'.actions.'Until-Control-Entity-Completed'.actions.'For-Each-Page'.actions.'Log-Get-Entity-Page'.inputs.body = $logGetEntityPageBodyTemplate
+
+$logEntityPageRecordCountBodyTemplate = $a.resources[0].properties.definition.actions.'Scope-Entities'.actions.'For-Each-Entity'.actions.'Until-Control-Entity-Completed'.actions.'For-Each-Page'.actions.'Validate-Get-Entity-Page'.actions.'Log-Entity-Page-Record-Count'.inputs.body
+$logEntityPageRecordCountBodyTemplate = $logEntityPageRecordCountBodyTemplate.Replace('"', '\"').Replace("`n","").Replace("`r","")
+$a.resources[0].properties.definition.actions.'Scope-Entities'.actions.'For-Each-Entity'.actions.'Until-Control-Entity-Completed'.actions.'For-Each-Page'.actions.'Validate-Get-Entity-Page'.actions.'Log-Entity-Page-Record-Count'.inputs.body = $logEntityPageRecordCountBodyTemplate
 
 #
 # PLEASE NOTE: the values of the Status* Properties are dependent on the Dfe.CdcEventApi.Domain.Models.ControlState Enum values
